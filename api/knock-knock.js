6d71532fa61d4d754f1d33ad21239c20bd3cc206
PorterK <whyisumad@me.com>
2016-09-28 10:53:29 -0400

    Documented code

--- api/knock-knock.js
+++ api/knock-knock.js
@@ -5,14 +5,21 @@ const bodyParser = require('body-parser');
 const models = require('../models');
 
 let router = express.Router();
+//Use JSON to parse requests and to send responses
 router.use(bodyParser.json());
 
+//If a GET request is sent to the endpoint
 router.get('/', (req, res)=>{
-
+  //Find all of the knock knock jokes
   models.knockJoke.findAll()
-  .then((jokes)=>{
+  .then((jokes=>{
+    //and send them
     res.send(jokes);
   })
+  .catch(err=>{
+    //on error, send the 500 (internal server error) code and the error message
+    res.status(500).send(err);
+  });
 
 });
 
